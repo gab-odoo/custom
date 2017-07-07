52c9353157877006710dbf4f0e19a40f79a77acb
gab-odoo <gab@odoo.com>
2017-07-07 18:20:40 +0200

    [IMP] Fixed table headers

---
+++ website_b2b/static/src/js/layout_improvements.js
@@ -0,0 +1,17 @@
+odoo.define('feuerwear.better_layout', function(require) {
+
+    var base = require('web_editor.base');
+
+    //var editor = require('base.editor');
+    if (!$('table#uhelp').length) {
+        return;
+    }
+
+    $(window).on('resize', function(event) {
+        var fixed_height = $('#uhelp').height();
+        $('.responsive-margin-top').css("margin-top",fixed_height+170)
+        $('.responsive-background').css("height",fixed_height+180)
+        $('.fixed-header').css("top",fixed_height+190)
+    });
+});
+
